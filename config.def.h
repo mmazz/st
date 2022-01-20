2f3cbab20aa7f981653c3fb6c94779bbd1ab8ef3
mmazz <mazzantimatiass@gmail.com>
2022-01-20 17:07:15 -0300

    xreosurces colors fix

--- config.def.h
+++ config.def.h
@@ -139,9 +139,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor, selection
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 256;
 unsigned int selectionbg = 257;
 unsigned int selectionfg = 7;
